a005f247a696c01cdec1ac6b1d157405ddf16e24
Dani Akash <DaniAkash@users.noreply.github.com>
2019-11-10 12:02:56 +0530

    updated comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-const net = require('net');
+const net = require("net");
 
+// Following code will be excecuted when client is connected
 const server = net.createServer(client => {
+  /**
+   * `client` object is an event emitter
+   */
+
   console.log("client connected");
+
   client.on("data", data => {
     console.log("data received:");
     console.log(data.toString());
   });
+  
   client.on("end", () => {
     console.log("client disconnected");
   });
